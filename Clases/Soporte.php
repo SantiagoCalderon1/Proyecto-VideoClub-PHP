d36bf22d4aa2f6e639d5742abb6e3c1cc618b8e9
SantiagoCalderon1 <Santiago.contactoyt@gmail.com>
2024-10-10 18:44:46 +0200

    Archivos soporte.php y inicio.php modificados

---
+++ Clases/Soporte.php
@@ -0,0 +1,35 @@
+<?php
+class Soporte
+{
+    private static const IVA =  0.21;
+    public $titulo = "";
+    protected $numero = "";
+    private $precio = "";
+
+    public function __construct($titulo,  $numero, $precio)
+    {
+        $this->titulo = $titulo;
+        $this->numero = $numero;
+        $this->precio = $precio;
+    }
+
+    public function getPrecio()
+    {
+        return  $this->precio;
+    }
+
+    public function getPrecioConIva()
+    {
+        return  $this->precio * self::IVA;
+    }
+
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    public function muestraResumen()
+    {
+        echo "<br>".$this->titulo . "<br>" . $this->getPrecio() . " € " . "(IVA no incluido)";
+    }
+}
---
+++ inicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h1>Inicio PHP</h1>
+<?php
+//Código de prueba para Clase Soporte
+include_once "Clases/Soporte.php";
+
+$soporte1 = new Soporte("Tenet", 22, 3); 
+echo "<strong>" . $soporte1->titulo . "</strong>"; 
+echo "<br>Precio: " . $soporte1->getPrecio() . " euros"; 
+echo "<br>Precio IVA incluido: " . $soporte1->getPrecioConIVA() . " euros";
+$soporte1->muestraResumen();
+?>  
+</body>
+</html>
