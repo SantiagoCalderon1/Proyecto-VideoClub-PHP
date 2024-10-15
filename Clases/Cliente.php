b2d8af96e47be17dee866d5fa56f189bd4e4a13b
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-10-15 16:50:25 +0200

    Cliente completado y inicio2 a medias

--- Clases/Cliente.php
+++ Clases/Cliente.php
@@ -1,54 +1,98 @@
 <?php
+include_once "Soporte.php";
 
-class Cliente {
+class Cliente
+{
     public $nombre;
     private $numero;
-    private $soportesAlquilados[];
+    private $soportesAlquilados = [];
     private $numSoportesAlquilados;
     private $maxAlquilerConcurrente;
 
-    public funtion __construct($nombre, $numero, $soportesAlquilados, $numSoportesAlquilados, $maxAlquilerConcurrente = 3){
-        $this->nombre=$nombre;
-        $this->numero=$numero;
-        $this->soportesAlquilados=$soportesAlquilados;
-        $this->numSoportesAlquilados=$numSoportesAlquilados;
-        $this->maxAlquilerConcurrente=$maxAlquilerConcurrente;
+    public function __construct($nombre, $numero, $maxAlquilerConcurrente = 3)
+    {
+        $this->nombre = $nombre;
+        $this->numero = $numero;
+        $this->numSoportesAlquilados = 0;
+        $this->maxAlquilerConcurrente = $maxAlquilerConcurrente;
     }
 
-    public function getNumero() {
+    public function getNumero()
+    {
         return  $this->numero;
     }
 
-    public function setNumero($numero){
-        return $this->numero=$numero;
+    public function setNumero($numero)
+    {
+        return $this->numero = $numero;
     }
 
-    public function getNumSoportesAlquilados(){
+    public function getNumSoportesAlquilados()
+    {
         return  $this->numSoportesAlquilados;
     }
 
 
-    public function muestraResumen(){
+    public function muestraResumen()
+    {
         echo 'El cliente de nombre ' . $this->nombre . ' tiene un total de ' . $this->soportesAlquilados . ' alquileres.';
     }
 
-    public function tieneAlquilado(Soporte $s) : bool{
+    public function tieneAlquilado(Soporte $s): bool
+    {
 
-        foreach ($this->$soportesAlquilados as $soporte) {
-            if($soporte == $s){
+        foreach ($this->soportesAlquilados as $soporte) {
+            if ($soporte == $s) {
                 return true;
             }
         }
         return false;
     }
 
-    public function alquilar(Soporte $s) : bool{
-        if($this->tieneAlquilado($s)==false && (numSoportesAlquilados<$maxAlquilerConcurrente)){
-            $numSoportesAlquilados++;
-            $soportesAlquilados.push($s);
-            echo 'Se ha alquilado un juego';
+    public function alquilar(Soporte $s): bool
+    {
+        if ($this->tieneAlquilado($s) == false && ($this->numSoportesAlquilados < $this->maxAlquilerConcurrente)) {
+            $this->numSoportesAlquilados++;
+            array_push($this->soportesAlquilados, $s);
+            echo "<br> Alquilado soporte a: $this->nombre <br>";
+            $s->muestraResumen();
+            return true;
+        } else {
+            echo "<br> El cliente ya tiene alquilado el soporte $s->titulo <br>";
+            return false;
+        }
+    }
+
+    public function devolver(int $numSoporte): bool
+    {
+        $encontrado = false;
+        // Debe comprobar que el soporte estaba alquilado
+        foreach ($this->soportesAlquilados as $clave=>$soporte) {
+            if ($soporte instanceof Soporte) {
+                if ($soporte->getNumero() == $numSoporte) {
+                    $encontrado = true;
+                    unset($this->soportesAlquilados[$clave]); //borrar del array
+                }
+            }
+        }
+        //actualizar la cantidad de soportes alquilados.
+        if ($encontrado) {
+            $this->numSoportesAlquilados--;
+            echo 'Se ha completado la devolucion';
+            return true;
+        } else {
+            echo 'No esta alquilado';
+            return false;
+        }
+    }
+
+    //Informa de cuantos alquileres tiene el cliente y los muestra.
+    public function listarAlquileres()
+    {
+        echo "El cliente tiene $this->numSoportesAlquilados alquileres";
+
+        foreach ($this->soportesAlquilados as $soporte) {
+            $soporte->muestraResumen();
         }
     }
 }
-
-?>
---
+++ inicio2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    include_once "Clases/CintaVideo.php";
+    include_once "Clases/Dvd.php";
+    include_once "Clases/Juego.php";
+    include_once "Clases/Cliente.php";
+
+    //instanciamos un par de objetos cliente
+    $cliente1 = new Cliente("Bruce Wayne", 23);
+    $cliente2 = new Cliente("Clark Kent", 33);
+
+    //mostramos el número de cada cliente creado 
+    echo "<br>El identificador del cliente 1 es: " . $cliente1->getNumero();
+    echo "<br>El identificador del cliente 2 es: " . $cliente2->getNumero();
+
+    //instancio algunos soportes 
+    $soporte1 = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
+    //$soporte2 = new Juego("The Last of Us Part II", 26, 49.99, "PS4", 1, 1);  
+    $soporte3 = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
+    $soporte4 = new Dvd("El Imperio Contraataca", 4, 3, "es,en","16:9");
+    
+    //alquilo algunos soportes
+    $cliente1->alquilar($soporte1);
+    //$cliente1->alquilar($soporte2);
+    $cliente1->alquilar($soporte3);
+    //voy a intentar alquilar de nuevo un soporte que ya tiene alquilado
+    $cliente1->alquilar($soporte1);
+    //el cliente tiene 3 soportes en alquiler como máximo
+    //este soporte no lo va a poder alquilar
+    $cliente1->alquilar($soporte4);
+    //este soporte no lo tiene alquilado
+    $cliente1->devolver(4);
+    //devuelvo un soporte que sí que tiene alquilado
+    $cliente1->devolver(2);
+    //alquilo otro soporte
+    $cliente1->alquilar($soporte4);
+    //listo los elementos alquilados
+    $cliente1->listarAlquileres();
+    //este cliente no tiene alquileres
+    $cliente2->devolver(2);
+
+    ?>
+</body>
+</html>
