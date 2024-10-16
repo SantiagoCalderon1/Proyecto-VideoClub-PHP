53cb8c1c20e8f437d65a8f451b3f99583ea52e68
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-10-16 16:16:49 +0200

    cliente sin numero de identificacion de soporte empezado

--- inicio2.php
+++ inicio2.php
@@ -22,10 +22,10 @@
     echo "<br>El identificador del cliente 2 es: " . $cliente2->getNumero();
 
     //instancio algunos soportes 
-    $soporte1 = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
-    $soporte2 = new Juego("The Last of Us Part II", 26, 49.99, "PS4", 1, 1);  
-    $soporte3 = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
-    $soporte4 = new Dvd("El Imperio Contraataca", 4, 3, "es,en","16:9");
+    $soporte1 = new CintaVideo("Los cazafantasmas", 3.5, 107);
+    $soporte2 = new Juego("The Last of Us Part II", 49.99, "PS4", 1, 1);  
+    $soporte3 = new Dvd("Origen", 15, "es,en,fr", "16:9");
+    $soporte4 = new Dvd("El Imperio Contraataca", 3, "es,en","16:9");
     
     //alquilo algunos soportes
     $cliente1->alquilar($soporte1);
