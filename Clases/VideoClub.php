b65ee3aef4e5fdefda790c01c2b67547536ef183
SantiagoCalderon1 <Santiago.contactoyt@gmail.com>
2024-10-17 18:16:40 +0200

    Metodo inlcuir producto terminado

--- Clases/VideoClub.php
+++ Clases/VideoClub.php
@@ -10,30 +10,39 @@ class VideoClub
     public function __construct($nombre, $productos, $socios)
     {
         $this->nombre = $nombre;
-        $this->numProductos++;
-        $this->numSocios++;
     }
 
     //Anthony
-    private function incluirProducto(producto $Soporte) {}
+    private function incluirProducto(Soporte $producto) {
+        array_push($this->productos,$producto);
+        $this->numProductos++;
+        echo "Incluido soporte ".$producto->getNumero();
+
+    }
     
     //Santiago
     public function incluirCintaVideo($titulo, $precio, $duracion) {
-        array_push($this->productos, new CintaVideo($titulo, $precio, $duracion))
-        $this->numProductos++;
+        incluirProducto(new CintaVideo($titulo, $precio, $duracion));
     }
     
     //Anthony
     public function incluirDvd($titulo, $precio, $idiomas, $pantalla) {}
     
     //Santiago
-    public function incluirJuego($titulo, $precio, $consola, $minJ, $maxJ) {}
+    public function incluirJuego($titulo, $precio, $consola, $minJ, $maxJ) {
+        incluirProducto(new  Juego($titulo, $precio, $consola, $minJ, $maxJ));
+    }
     
     //Anthony
     public function incluirSocio($nombre, $maxAlquileresConcurrentes = 3) {}
 
     //Santiago
     public function listarProductos() {
+        $contador = 0;
+        foreach ($this->productos as $producto) {
+            $contador++;
+            echo $contador.".-".
+        }  
     }
 
     //Anthony
