c21069d476a6df195e5fba0dc12797c2cf83e6bc
SantiagoCalderon1 <Santiago.contactoyt@gmail.com>
2024-10-24 19:25:12 +0200

    Terminar el auotload

--- autoload.php
+++ autoload.php
@@ -1,13 +1,7 @@
 <?php
-spl_autoload_register(function ($nombreClase) {
-    // Ruta a la carpeta "app" desde el directorio principal
-    $rutaArchivo =  $nombreClase . '.php';
-    $rutaArchivo = str_replace("\\", "/", $rutaArchivo); // Sustituimos las barras
-
-    // Verificar si el archivo de la clase existe antes de incluirlo
-    if (file_exists($rutaArchivo)) {
-        include_once $rutaArchivo;
-    } else {
-        echo "La clase $nombreClase no pudo ser cargada. El archivo $rutaArchivo no existe.";
-    }
-});
+spl_autoload_register(function( $nombreClase ) {
+    $ruta = "app\\".$nombreClase.'.php';
+    $ruta = str_replace("\\", "/", $ruta); // Sustituimos las barras
+    include_once '$ruta';
+} );
+?>
--- test/inicio3.php
+++ test/inicio3.php
@@ -1,6 +1,6 @@
 
     <?php
-    require_once("../autoload.php");
+   include("../autoload.php");
 
     use ProyectoVideoClub\app\VideoClub;
 
